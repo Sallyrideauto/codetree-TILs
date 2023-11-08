0b0f37b7378df5c3341e19737455b9e736e90524
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-08 20:07:06 +0900

    Title: 숨은 단어 찾기 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 231108/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 231108/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -4,15 +4,17 @@ grid = [input() for _ in range(N)]
 
 # LEE를 찾는 함수 정의
 def search_LEE(x, y, dx, dy):
-    try:
-        # 주어진 방향으로 LEE가 나오는지 확인
-        for i in range(3):
-            if grid[x + dx * i][y + dy * i] != 'LEE'[i]:
-                return False
-        return True
-    except IndexError:
-        # 인덱스 에러가 발생하면 범위 밖이므로 False 반환
-        return False
+    for i in range(3):
+        # 새로운 탐색 위치
+        new_x, new_y = x + dx * i, y + dy * i
+        # 그리드 범위를 벗어나면 바로 False 반환
+        if new_x < 0 or new_y < 0 or new_x >= N or new_y >= M:
+            return False
+        # 그리드 범위 내에 있지만 LEE 문자열과 매치되지 않으면 False 반환
+        if grid[new_x][new_y] != 'LEE'[i]:
+            return False
+    # 모든 검사를 통과하면 True 반환
+    return True
 
 # 모든 위치에 대해 8가지 방향 탐색
 count = 0
