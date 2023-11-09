bc36f9027da90ba98c65fdd08cc69f77e9655bf7
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-09 19:11:32 +0900

    Title: 상해버린 치즈, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/상해버린 치즈/rotten-cheese.py
+++ 231109/상해버린 치즈/rotten-cheese.py
@@ -1,34 +1,41 @@
-# 사람 수, 치즈 수, 치즈를 먹은 기록의 수, 아픈 기록의 수 입력 받기
+'''
+이 문제를 해결하기 위한 방법을 단계별로 설명하겠습니다. 
+우리는 이 정보를 사용하여 상한 치즈를 먹고 아플 가능성이 있는 최대 인원 수를 추정해야 합니다.
+
+1. 각 치즈마다 먹은 시간을 추적하며, 각 사람이 아프기 시작한 정확한 시간을 기록합니다.
+2. 아픈 기록으로부터 역추적하여, 해당 시간 이전에 먹은 치즈들 중 어느 것이 상한 치즈일 가능성이 있는지 파악합니다.
+3. 아픈 사람이 먹은 치즈를 먹은 다른 모든 사람들도 아플 가능성이 있다고 간주합니다.
+4. 모든 아픈 사람의 기록에 대해 이러한 과정을 반복하여, 상한 치즈를 먹었을 가능성이 있는 모든 사람들의 목록을 만듭니다.
+5. 이 목록에 있는 사람 수가 필요한 약의 최대 개수가 됩니다.
+'''
+
+# 입력 받기
 N, M, D, S = map(int, input().split())
+cheese_logs = [list(map(int, input().split())) for _ in range(D)]
+sick_logs = [list(map(int, input().split())) for _ in range(S)]
 
-# 각 사람이 어떤 치즈를 언제 먹었는지 저장
-people_eat_records = {i: [] for i in range(1, N + 1)}
-for _ in range(D):
-    p, m, t = map(int, input().split())
-    people_eat_records[p].append((m, t))
+# 각 치즈를 누가 언제 먹었는지 저장하는 딕셔너리를 초기화합니다.
+cheese_eaten = {m: [] for m in range(1, M+1)}
 
-# 아픈 사람과 그 시간을 저장
-sick_records = {i: 101 for i in range(1, N + 1)}    # 초기값은 아픈 적이 없는 것으로 설정
-for _ in range(S):
-    p, t = map(int, input().split())
-    sick_records[p] = t
+# 치즈 먹은 기록을 저장합니다.
+for log in cheese_logs:
+    p, m, t = log
+    cheese_eaten[m].append((p, t))
 
-# 상한 치즈 후보를 확인
-bad_cheese_candidates = set(range(1, M + 1))
+# 상한 치즈를 먹었을 가능성이 있는 사람들의 집합입니다.
+possible_sick_people = set()
 
-for person, sick_time in sick_records.items():
-    # 아픈 사람이 먹었던 치즈 중, 아픈 시간 이전에 먹은 치즈만 후보에 포함
-    valid_cheese = {m for m, t in people_eat_records[person] if t < sick_time}
-    bad_cheese_candidates &= valid_cheese   # 교집합으로 후보 갱신
+# 아픈 기록을 통해 상한 치즈를 역추적합니다.
+for log in sick_logs:
+    p, t = log
+    # 해당 사람이 아픈 시간 이전에 먹은 모든 치즈를 확인합니다.
+    for m, eaten_times in cheese_eaten.items():
+        for e_p, e_t in eaten_times:
+            # 아픈 사람이 먹은 시간과 치즈를 먹은 시간을 비교합니다.
+            if e_p == p and e_t < t:
+                # 이 치즈를 먹은 다른 모든 사람도 상한 치즈를 먹었을 가능성이 있습니다.
+                for other_p, _ in eaten_times:
+                    possible_sick_people.add(other_p)
 
-# 후보 중 하나라도 먹은 사람을 카운트
-potential_sick = set()
-
-for cheese in bad_cheese_candidates:
-    for person, records in people_eat_records.items():
-        for m, t in records:
-            if m == cheese and t < sick_records[person]:
-                potential_sick.add(person)
-
-# 가능한 아픈 사람들의 수를 출력
-print(len(potential_sick))
+# 필요한 약의 최대 개수를 출력합니다.
+print(len(possible_sick_people))
