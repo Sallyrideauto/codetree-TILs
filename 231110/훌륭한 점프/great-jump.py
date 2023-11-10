a0966a1a6a096808afd73513549f8b43091c713d
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-10 16:59:55 +0900

    Title: 훌륭한 점프, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231110/훌륭한 점프/great-jump.py
+++ 231110/훌륭한 점프/great-jump.py
@@ -1,32 +1,34 @@
 '''
-문제를 해결하기 위해 다른 접근 방법을 사용해야 할 것 같습니다.
-이 문제는 ‘슬라이딩 윈도우’ 방식으로 해결할 수 있습니다.
-이 방식은 각 구간 내에서 최댓값과 최솟값의 차이를 계산하여, 이 차이가 조건을 만족하는 최대 구간의 크기를 찾는 데 사용됩니다.
+이 문제를 해결하기 위해 다른 접근 방식이 필요합니다. 
+문제의 목표는 거리 k 이내로 점프하며 n번 돌에 도달할 때, 거쳐간 지점에 적힌 숫자들 중 최댓값을 최소화하는 것입니다.
 
-문제의 요구 사항에 따라, 우리는 각 돌을 건너뛰며 이동할 때 거쳐간 돌들 중 최댓값이 최소가 되도록 해야 합니다. 
-이를 위해 다음과 같은 접근 방식을 사용할 수 있습니다:
+이 문제를 해결하기 위한 접근 방법은 다음과 같습니다:
 
-1. 각 돌 사이의 가능한 거리를 계산합니다.
-2. 이 거리들 중 최댓값을 최소화하는 구간을 찾습니다.
-3. 이 구간의 크기가 최대가 되도록 합니다.
+1. 가능한 모든 점프 경로를 고려하여, 각 경로에서 거쳐가는 숫자들 중 최댓값을 찾습니다.
+2. 이 최댓값들 중 최솟값을 찾습니다.
 
-이 코드는 가능한 모든 거리를 계산한 후 이를 내림차순으로 정렬하고, k번째로 큰 거리를 최소 최대 거리로 선택합니다. 
-이것은 k번째 점프에서 가능한 최대 거리를 최소화하는 것과 같습니다.
+이 코드는 각 시작점에서 최대 k 만큼 점프할 수 있는 모든 경로를 고려하여, 그 경로에서의 최댓값을 찾은 다음 그 중 최솟값을 반환합니다.
+이는 주어진 조건 하에서 최대한 거리를 두며 이동할 때, 거쳐가는 숫자들 중 최댓값을 최소화하는 경로를 찾는 것입니다.
 '''
 
 n, k = map(int, input().split())
 stones = list(map(int, input().split()))
 
-def min_max_distance(stones, k):
-    # 거리들을 저장할 리스트
-    distances = []
-    for i in range(n - 1):
-        for j in range(i + 1, min(i + k + 1, n)):
-            # 각 돌 사이의 가능한 거리 계산
-            distances.append(stones[j] - stones[i])
+# 모든 경로를 탐색하여 각 경로의 최댓값 중 최솟값을 찾는 함수
+def find_min_max(stones, k, n):
+    min_max = float('inf')
 
-    # 최소 최대 거리 탐색
-    distances.sort(reverse = True)
-    return distances[k - 1]
+    # 모든 가능한 시작 위치에서 탐색
+    for start in range(n - 1):
+        max_in_path = stones[start]
 
-print(min_max_distance(stones, k))
+        # 최대 k만큼 점프
+        for end in range(start + 1, min(start + k + 1, n)):
+            max_in_path = max(max_in_path, stones[end])
+            # 마지막 돌에 도달하면 최대값을 갱신
+            if end == n - 1:
+                min_max = min(min_max, max_in_path)
+
+    return min_max
+
+print(find_min_max(stones, k, n))
