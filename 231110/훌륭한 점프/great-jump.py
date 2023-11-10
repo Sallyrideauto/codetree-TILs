37bd80f5cfa71161f7e62be2da58646d683ceb0b
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-10 16:47:32 +0900

    Title: 훌륭한 점프, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231110/훌륭한 점프/great-jump.py
+++ 231110/훌륭한 점프/great-jump.py
@@ -1,40 +1,44 @@
 '''
-문제에 대한 이해를 바탕으로, 코드의 접근 방식을 다시 검토해보겠습니다. 
-목표는 거리 k 이내로 점프하면서, 거쳐간 지점에 적혀있는 숫자들 중 최댓값을 최소로 하는 것입니다.
+제공된 입력 케이스에서 예상되는 출력이 올바르게 나오지 않는 것을 감안하여, 문제 해결 방식을 다시 살펴보겠습니다. 
+목표는 각 돌을 건너뛰며 이동할 때, 거쳐간 돌들 중 최댓값이 최소가 되도록 하는 것입니다.
 
-이를 위해선 두 가지 중요한 점을 고려해야 합니다:
-1. 거리 k 이내에 점프할 수 있는지 확인.
-2. 가능한 최대값을 최소로 만들 수 있는 경로 찾기.
+이를 위해서는 다음과 같은 방식으로 접근할 수 있습니다:
+1. 가능한 모든 숫자들의 최댓값을 고려하여 이진 탐색을 사용합니다.
+2. 각 이진 탐색 단계에서, 해당 최댓값을 초과하지 않는 한에서 최대한 멀리 떨어진 돌로 점프해야 합니다.
+3. 만약 이러한 점프가 불가능하면 최댓값을 증가시켜야 하고, 가능하다면 최댓값을 감소시킬 수 있습니다.
 
-이진 탐색을 사용하는 현재의 접근 방식은 올바른 방향이지만, 구현에서 문제가 있습니다. 
-can_reach 함수가 주어진 최댓값으로 n번 돌까지 이동할 수 있는지를 확인하는 로직을 개선할 필요가 있습니다.
-
-각 돌의 값이 최댓값보다 작거나 같을 때만 해당 돌로 이동할 수 있어야 합니다. 
-이동할 수 있는 돌이 없을 경우는 최댓값을 늘려야 하며, n번 돌까지 도달할 수 있을 경우는 최댓값을 줄일 수 있습니다.
+이 코드는 각 이진 탐색 단계에서 최댓값을 기준으로 n번 돌까지 도달할 수 있는지 검사합니다. 
+can_reach 함수는 주어진 최댓값을 초과하지 않는 한에서 최대한 멀리 떨어진 돌로 점프하는 것을 시도합니다. 
+이진 탐색은 최적의 최댓값을 찾기 위해 사용됩니다.
 '''
 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 def can_reach(max_val):
-    current = 0
-    while current < n - 1:
-        next_jump = min(current + k, n - 1)
-        while next_jump > current and arr[next_jump] > max_val:
-            next_jump -= 1
-        if next_jump == current:
+    current_pos = 0
+    for _ in range(n):  # 최대 n번 점프
+        next_pos = current_pos
+        for jump in range(1, k + 1):  # 최대 k 거리만큼 점프
+            if current_pos + jump >= n or arr[current_pos + jump] > max_val:
+                break
+            next_pos = current_pos + jump
+        if next_pos == current_pos:  # 더 이상 점프할 수 없는 경우
             return False
-        current = next_jump
-    return True
+        current_pos = next_pos
+        if current_pos == n - 1:  # 마지막 돌에 도달한 경우
+            return True
+    return False
 
-low, high = 1, max(arr)
-min_max_val = high
+# 이진 탐색으로 최적의 최대값 찾기
+low, high = min(arr), max(arr)
+result = high
 while low <= high:
     mid = (low + high) // 2
     if can_reach(mid):
-        min_max_val = mid
+        result = mid
         high = mid - 1
     else:
         low = mid + 1
 
-print(min_max_val)
+print(result)
