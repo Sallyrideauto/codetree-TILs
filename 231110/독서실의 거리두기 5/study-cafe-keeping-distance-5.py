99ad664839fa29c643022647ba8729dd6849d1f6
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-10 23:32:23 +0900

    Title: 독서실의 거리두기 5, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231110/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 231110/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,16 +1,25 @@
 N = int(input())
 seats = input().strip()
 
-distances = []
+# 최소 거리 계산
+def calc_min_dist(distances):
+    return max((d + 1) // 2 for d in distances)
 
-for i in range(1, len(seats)):
-    if seats[i - 1] == '1' and seats[i] == '0':
-        dist = 0
-        j = i
-        while j < len(seats) and seats[j] == '0':
-            dist += 1
-            j += 1
-        distances.append(dist)
+# 1 사이의 거리 계산
+def calc_distances(seats):
+    distances = []
+    last_seat = -1
+    for i, seat in enumerate(seats):
+        if seat == '1':
+            if last_seat != -1:
+                distances.append(i - last_seat - 1)
+            last_seat = i
+    # 시작점과 첫 번째 '1' 사이, 마지막 '1'과 끝점 사이의 거리 추가
+    if seats[0] == '0':
+        distances.append(last_seat)
+    if seats[-1] == '0':
+        distances.append(len(seats) - last_seat - 1)
+    return distances
 
-updated_distances = [x - 1 for x in distances]
-print(max(updated_distances))
+distances = calc_distances(seats)
+print(calc_min_dist(distances))
