127976227bcc872c36a837a7093cfbfd82752f03
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-13 20:02:44 +0900

    Title: 구역 청소, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231113/구역 청소/district-cleaning.py
+++ 231113/구역 청소/district-cleaning.py
@@ -1,9 +1,13 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-# 청소된 구역의 시작과 끝 계산
-start = min(a, c)
-end = max(b, d)
+# 겹치는 구간의 시작점은 두 구간의 시작점 중 더 큰 값
+overlap_start = max(a, c)
+# 겹치는 구간의 끝점은 두 구간의 끝점 중 더 작은 값
+overlap_end = min(b, d)
 
-# 청소된 구역의 수 출력
-print(end - start)
+# 청소된 영역 계산
+# 겹치지 않는 A의 부분 + 겹치지 않는 B의 부분 + 겹치는 부분
+cleaned_area = (b - a + 1) + (d - c + 1) - max(0, (overlap_end - overlap_start + 1))
+
+print(cleaned_area - 1)
