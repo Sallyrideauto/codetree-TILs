f78e9adba25102e726cba9255cefb33285189154
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-13 19:49:17 +0900

    Title: 두 직사각형, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/두 직사각형/two-rectangles.py
+++ 231113/두 직사각형/two-rectangles.py
@@ -1,5 +1,6 @@
-x1, x2, y1, y2 = map(int, input().split())
-a1, a2, b1, b2 = map(int, input().split())
+# 입력 받기
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
 
 # 겹치는 조건 확인
 if x2 < a1 or x1 > a2 or y2 < b1 or y1 > b2:
