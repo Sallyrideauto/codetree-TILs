f26c33218b24f3d3d8865218fd859758ba5d889a
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-16 22:56:30 +0900

    Title: 연속된 숫자 만들기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 231116/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -2,21 +2,20 @@ def continuous_nums(a, b, c):
     # 세 위치를 정렬
     positions = sorted([a, b, c])
 
-    # 왼쪽과 중간, 오른쪽과 중간 사이의 간격을 계산
-    left_gap = positions[1] - positions[0]
-    right_gap = positions[2] - positions[1]
+    # 각 사이의 간격 계산
+    gap1 = positions[1] - positions[0]
+    gap2 = positions[2] - positions[1]
 
     # 이미 연속된 숫자 위치에 있는 경우
-    if left_gap == 1 or right_gap == 1:
+    if gap1 == 1 and gap2 == 1:
         return 0
-
-    # 한 명만 이동하는 경우(한쪽 간격이 2)
-    if left_gap == 2 or right_gap == 2:
+    
+    # 한 쪽 간격이 1이고 다른 쪽 간격이 2 이상인 경우
+    if gap1 == 1 or gap2 == 1:
         return 1
 
-    # 두 명이 이동해야 하는 경우
+    # 두 간격 모두 2 이상인 경우
     return 2
-
 
 # 함수 테스트
 a, b, c = map(int, input().split())
