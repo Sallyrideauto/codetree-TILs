657c7829cc7d63c40834611643f34ff8659fea9c
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-21 23:43:23 +0900

    Title: 독서실의 거리두기 3, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231121/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 231121/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -4,21 +4,23 @@ def max_distance(N, seats):
 
     # 현재 간격을 측정하기 위한 변수
     current_gap = 0
+    
+    # 첫 번째 사람을 만날 때까지의 간격
+    first_person = seats.find('1')
+    last_person = seats.rfind('1')
 
-    for seat in seats:
+    # 첫 번째와 마지막 사람 사이의 간격만 고려
+    for i in range(first_person, last_person + 1):
         # 현재 좌석이 비어 있으면 간격을 증가시킴
-        if seat == '0':
+        if seats[i] == '0':
             current_gap += 1
         else:
             # 좌석이 차 있으면 최대 간격 업데이트 후 간격 초기화
             max_gap = max(max_gap, current_gap)
             current_gap = 0
 
-    # 마지막 구간의 간격도 확인
-    max_gap = max(max_gap, current_gap)
-
-    # 새로운 사람을 추가했을 때 최대 거리 계산
-    return (max_gap + 1) // 2
+    # 새로운 사람을 추가했을 때의 최대 거리 계산
+    return max(first_person, (max_gap + 1) // 2, N - last_person - 1)
 
 N = int(input())
 seats = input()
