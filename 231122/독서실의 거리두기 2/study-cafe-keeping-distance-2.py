7e5a40e4540c467ce1612571c27bc789aab4e9d3
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-22 17:56:47 +0900

    Title: 독서실의 거리두기 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231122/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 231122/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -1,22 +1,31 @@
 def max_distance_between_people(N, seating):
-    # 빈 공간 찾기
-    empty_spaces = []
-    last_person = -1
-    for i, seat in enumerate(seating):
-        if seat == '1':
-            if last_person != -1:
-                empty_spaces.append((last_person, i))
-            last_person = i
-    
-    # 각 구간에서의 최대 거리 계산
+    # 각 구간의 시작과 끝을 찾는 함수
+    def find_empty_spaces(seating):
+        spaces = []
+        start = None
+        for i, seat in enumerate(seating):
+            if seat == '0' and start is None:
+                start = i
+            elif seat == '1' and start is not None:
+                spaces.append((start, i - 1))
+                start = None
+        if start is not None:
+            spaces.append((start, N - 1))
+        return spaces
+
+    # 최대 거리 계산
     max_distance = 0
+    empty_spaces = find_empty_spaces(seating)
     for start, end in empty_spaces:
-        # 양쪽 끝에 사람이 앉아 있는 경우
-        if start != 0 and end != N - 1:
-            distance = (end - start) // 2
-        # 한쪽 끝에만 사람이 앉아 있는 경우
+        # 구간의 길이 계산
+        length = end - start + 1
+
+        # 양쪽 끝에 사람이 없는 경우 (공간 전체 사용)
+        if start == 0 or end == N - 1:
+            distance = length
+        # 양쪽 끝에 사람이 있는 경우 (공간 절반 사용)
         else:
-            distance = end - start - 1
+            distance = (length + 1) // 2
         max_distance = max(max_distance, distance)
 
     return max_distance
