1d25b3edbc1b02deb491d88cfbae0c0e5d6cecc1
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-11-24 18:28:15 +0900

    Title: 두 번째로 작은 숫자의 위치, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 231124/두 번째로 작은 숫자의 위치/location-of-the-second-smallest-number.py
+++ 231124/두 번째로 작은 숫자의 위치/location-of-the-second-smallest-number.py
@@ -13,10 +13,17 @@ for num in sorted_array:
         second_min = num
         break
 
-# 두 번째로 작은 숫자의 위치 탐색
+# 두 번째로 작은 숫자의 위치와 유일성을 확인
 if second_min is not None:
-    position = original_array.index(second_min) + 1
-    print(position)
+    # 원본 배열에서 두 번째로 작은 숫자의 모든 인덱스 탐색
+    positions = [i for i, x in enumerate(original_array) if x == second_min]
+
+    # 두 번째로 작은 숫자가 하나만 있으면 그 위치를 출력
+    if len(positions) == 1:
+        print(positions[0] + 1)
+    else:
+        # 두 번쨰로 작은 숫자가 여러 개 있는 경우 -1 출력
+        print(-1)
 else:
     # 두 번째로 작은 숫자가 없는 경우 -1 출력
     print(-1)
