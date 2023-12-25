2b66f33ed39941f3ac8d7485fabe49991d8a6e6f
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2023-12-25 19:22:44 +0900

    Title: 전부 포함하는 선분, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231225/전부 포함하는 선분/all-inclusive-segment.py
+++ 231225/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,37 +1,38 @@
-def min_length_after_removing_one_segment(n, segments):
+def min_length_after_removal(n, segments):
     """
-    Calculate the minimum length of the shortest line segment that can contain
-    all the remaining n - 1 line segments after removing one segment.
+    Calculate the minimum length of the shortest line segment that can contain all 
+    the remaining segments after removing one segment.
 
     :param n: Number of line segments
     :param segments: List of tuples representing the line segments (x1, x2)
-    :return: Minimum length of the line segment to satisfy the condition
+    :return: Minimum length of the encompassing line segment after removal
     """
-    # Sort the segments based on the starting point
-    segments.sort()
+    # Sort the segments based on their starting and ending points
+    segments.sort(key=lambda x: (x[0], x[1]))
 
-    # Initialize the minimum length required to encompass all segments except one
-    min_length = float('inf')
+    # Find the overall min start point and max end point
+    min_start = segments[0][0]
+    max_end = max(segments, key=lambda x: x[1])[1]
 
-    # Calculate the initial total length
-    total_length = segments[-1][1] - segments[0][0]
+    # Initialize the minimum length after removal
+    min_length = max_end - min_start
 
-    # Iterate over the segments and calculate the length if each segment is removed
+    # Calculate the length if each segment were removed
     for i in range(n):
         if i == 0:
-            # Length without the first segment
-            current_length = segments[-1][1] - segments[1][0]
-        elif i == n - 1:
-            # Length without the last segment
-            current_length = segments[-2][1] - segments[0][0]
+            # If the first segment is removed
+            new_min_start = segments[1][0]
         else:
-            # Length without the i-th segment
-            # Combine the segments before and after the i-th segment
-            current_length = (segments[i - 1][1] - segments[0][0]) + (segments[-1][1] - segments[i + 1][0])
-            current_length = min(current_length, total_length - (segments[i][1] - segments[i][0]))
+            new_min_start = min_start
 
-        # Update the minimum length
-        min_length = min(min_length, current_length)
+        if i == n - 1:
+            # If the last segment is removed
+            new_max_end = segments[-2][1]
+        else:
+            new_max_end = max(segments[i+1][1], max_end)
+
+        # Update the minimum length after removal
+        min_length = min(min_length, new_max_end - new_min_start)
 
     return min_length
 
@@ -42,6 +43,6 @@ for _ in range(n):
     x1, x2 = map(int, input().split())
     segments.append((x1, x2))
 
-min_length = min_length_after_removing_one_segment(n, segments)
+min_length = min_length_after_removal(n, segments)
 
 print(min_length)
