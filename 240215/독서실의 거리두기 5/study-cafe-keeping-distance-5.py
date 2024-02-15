ac5dc90d8f9c03531c89f983a13c19210f009d85
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-02-15 15:19:59 +0900

    Title: 독서실의 거리두기 5, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240215/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240215/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -4,24 +4,26 @@ seats = input()   # 좌석 상태를 나타내는 문자열
 
 # 가장 가까운 두 사람의 거리의 최댓값을 저장할 변수
 max_distance = 0
-# 현재 사람을 앉히면서 확인하는 공석의 개수
-current_distance = 0
+# 사람이 앉을 수 있는 시작 인덱스를 저장할 변수
+start_index = 0
 
 # 좌석 상태를 순회하며 최대 거리 계산
-for seat in seats:
-    # 공석인 경우
-    if seat == '0':
-        # 현재 공석의 개수 증가
-        current_distance += 1
+for i in range(N):
+    # 사람이 앉지 않은 경우
+    if seats[i] == '0':
+        # 시작 인덱스가 설정되지 않은 경우, 시작 인덱스를 설정
+        if start_index == 0:
+            start_index = i
     # 사람이 앉은 경우
     else:
-        # 최대 거리 업데이트
-        max_distance = max(max_distance, current_distance)
-        # 현재 공석 개수 초기화
-        current_distance = 0
+        # 시작 인덱스가 설정되어 있을 때, 최대 거리 갱신
+        if start_index != 0:
+            max_distance = max(max_distance, (i - start_index) // 2)
+            start_index = 0
 
 # 마지막 좌석까지의 거리를 고려하여 최대 거리 업데이트
-max_distance = max(max_distance, current_distance)
+if start_index != 0:
+    max_distance = max(max_distance, N - 1 - start_index) + 1
 
 # 최대 거리 출력
-print((max_distance + 1) // 2)
+print(max_distance)
