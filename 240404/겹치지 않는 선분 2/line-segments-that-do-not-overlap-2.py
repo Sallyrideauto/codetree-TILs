26a7d51e8cefbc7cc3d52975b42fec5e647194eb
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-04-04 21:43:18 +0900

    Title: 겹치지 않는 선분 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240404/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240404/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,10 +1,10 @@
 def solution():
     n = int(input())
     lines = [list(map(int, input().split())) for _ in range(n)]
-    lines.sort()  # 선분을 시작점과 끝점을 동시에 고려하여 정렬합니다.
+    lines.sort(key=lambda x: x[0])  # 시작점을 기준으로 선분을 정렬합니다.
 
     cnt = 0
-    end = 0
+    end = -1
     for line in lines:
         if line[0] > end:
             cnt += 1
