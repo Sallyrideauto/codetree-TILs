5d6cf501db39eef1ba7d9b84bc7c22f82fe1077f
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-07-31 23:10:52 +0900

    Title: 홀수 짝수의 묶음, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240731/홀수 짝수의 묶음/odd-even-bundle.py
@@ -1,32 +1,36 @@
-N = int(input())
-arr = list(map(int, input().split()))
-
-even, odd = 0, 0
-
-cnt = 0
-ans = 0
-
-for i in range(N):
-    if len(arr) % 2 != 0:
-        odd += 1
-    else:
-        even += 1
-        
-if even > odd:
-    ans = odd * 2 + 1
-elif even == odd:
-    ans = even + odd
-else:
-    ans = even * 2
-    size = odd - even
+def main():
+    n = int(input())
+    arr = list(map(int, input().split()))
     
-    if size % 3 == 0:
-        ans += (size / 3) * 2
-    else:
-        if (size % 3) % 2 == 0:
-            ans += size / 3 * 2 + 1
+    ans = 0
+
+    odd = 0
+    even = 0
+
+    for i in range(n):
+        num = arr[i]
+        if num % 2 != 0:
+            odd += 1
         else:
-            ans += size / 3 * 2 - 1
-            
-ans = int(ans)
-print(ans)
+            even += 1
+
+    if even > odd:
+        ans = odd * 2 + 1
+    elif even == odd:
+        ans = even + odd
+    else:
+        ans = even * 2
+        size = odd - even
+
+        if size % 3 == 0:
+            ans += (size // 3) * 2
+        else:
+            if (size % 3) % 2 == 0:
+                ans += (size // 3) * 2 + 1
+            else:
+                ans += (size // 3) * 2 - 1
+
+    print(ans)
+
+if __name__ == "__main__":
+    main()
