5489146a5ea060cac94f3f23a7f6b850093ed8dd
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-01 17:34:58 +0900

    Title: 2개씩 그룹짓기 2, Time: 209ms, Memory: 40MB, Status: Passed - CodeTree

--- 240801/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240801/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -1,16 +1,19 @@
+import sys
+si = sys.stdin.readline
+
 def max_min_pair_difference(n, nums):
     # 입력 리스트 정렬
     nums.sort()
     
     # 인접한 숫자끼리 그룹화하여 각 그룹의 차이 계산
-    min_differences = []
-    for i in range(len(nums)):
-        min_differences.append(nums[i - 1] - nums[i])
+    min_differences = sys.maxsize
+    for i in range(1, n + 1):
+        min_differences = min(min_differences, nums[n + i] - nums[i])
         
     # 각 그룹의 차이 중 최소값 구하기
-    return abs(min(min_differences))
+    return min_differences
     
-n = int(input().strip())
-nums = list(map(int, input().strip().split()))
+n = int(si())
+nums = [0] + list(map(int, si().split()))
 
 print(max_min_pair_difference(n, nums))
