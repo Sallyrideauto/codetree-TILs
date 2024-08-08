066324104487f87c7114593476213285f89f8e89
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-08 21:42:53 +0900

    Title: 초기 수열 복원하기, Time: 218ms, Memory: 73MB, Status: Passed - CodeTree

--- 240808/초기 수열 복원하기/restore-initial-sequence.py
+++ 240808/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,27 +1,42 @@
 def restore_sequence(N, sums):
-    from itertools import permutations
-
-    # 모든 가능한 수열을 생성
-    possible_sequences = permutations(range(1, N + 1))
-
-    # 사전순으로 가장 앞선 수열을 찾기
-    for sequence in possible_sequences:
-        valid = True
-        for i in range(N - 1):
-            if sequence[i] + sequence[i + 1] != sums[i]:
-                valid = False
-                break
-        if valid:
-            return sequence
+    # 수열을 저장할 리스트
+    result = [0] * N
+    used = [False] * (N + 1)
+    
+    def backtrack(index):
+        if index == N:
+            return True
+        
+        # 현재 합에서 가능한 다음 수를 계산
+        if index == 0:
+            for first in range(1, N + 1):
+                result[index] = first
+                used[first] = True
+                if backtrack(index + 1):
+                    return True
+                used[first] = False
+        else:
+            next_num = sums[index - 1] - result[index - 1]
+            if 1 <= next_num <= N and not used[next_num]:
+                result[index] = next_num
+                used[next_num] = True
+                if backtrack(index + 1):
+                    return True
+                used[next_num] = False
+        
+        return False
+    
+    backtrack(0)
+    return result
 
 def main():
     import sys
     input = sys.stdin.read
     data = input().split()
-
+    
     N = int(data[0])
     sums = list(map(int, data[1:]))
-
+    
     result = restore_sequence(N, sums)
     print(" ".join(map(str, result)))
 
