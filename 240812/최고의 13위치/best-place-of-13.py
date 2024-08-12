5690b83c7d483a06cf5230064f0d10ab174b1f49
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-12 21:32:56 +0900

    Title: 최고의 13위치, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240812/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 모든 쌍을 다 잡아 보기
+max_cnt = 0
+for i in range(n):
+    # 격자를 벗어나지 않을 범위로만 잡음
+    for j in range(n - 2):
+        cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+        max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
