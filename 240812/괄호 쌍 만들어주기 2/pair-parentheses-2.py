153bbe3579ddae6ee0fc10c910e27859bd4775d8
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-12 21:58:04 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240812/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240812/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -4,7 +4,7 @@ n = len(string)
 cnt = 0
 for i in range(n - 1):
     for j in range(i + 1, n - 1):
-        if string[i - 1] == '(' and string[i] == '(' and string[j] == ')' and string[j + 1] == ')':
+        if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
             cnt += 1
 
 print(cnt)
