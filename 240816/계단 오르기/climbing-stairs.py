5ac776ce5c8ff9780d76b929495a2f23a8c1ce78
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-16 14:02:36 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240816/계단 오르기/climbing-stairs.py
@@ -0,0 +1,20 @@
+def count_ways(n):
+    MOD = 10007
+
+    # dp 배열 초기화
+    dp = [0] * (n + 1)
+
+    # 초기 조건 설정
+    if n >= 2:
+        dp[2] = 1
+    if n >= 3:
+        dp[3] = 1
+
+    # dp 배열을 채워 나가기
+    for i in range(4, n + 1):
+        dp[i] = (dp[i - 2] + dp[t - 3]) % MOD
+
+    return dp[n]
+
+n = int(input())
+print(count_ways(n))
