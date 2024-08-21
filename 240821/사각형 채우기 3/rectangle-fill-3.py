0a57f105a9c678d3fba4abba9fb74b759b42f548
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-21 20:22:59 +0900

    Title: 사각형 채우기 3, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240821/사각형 채우기 3/rectangle-fill-3.py
+++ 240821/사각형 채우기 3/rectangle-fill-3.py
@@ -5,17 +5,16 @@ def tiling_ways(n):
         return 2
     elif n == 2:
         return 7
-
+    
     # dp 배열 초기화
     dp = [0] * (n + 1)
     
     # 초기 값 설정
     dp[1] = 2
     dp[2] = 7
-    dp[3] = 22
     
     # DP 점화식을 통한 값 채우기
-    for i in range(4, n + 1):
+    for i in range(3, n + 1):
         dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % MOD
     
     return dp[n]
