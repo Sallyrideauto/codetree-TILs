b6dd047212db7255e8a778d87ad56ac776cc1571
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-08-25 22:57:26 +0900

    Title: 최소공배수 구하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240825/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,10 @@
+def find_lcm(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+
+    print(n * m // gcd)
+
+n, m = map(int, input().split())
+find_lcm(n, m)
