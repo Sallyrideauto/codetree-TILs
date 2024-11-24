cbc515b48db0d1a2422ae2a3ec4a61d58612c6ec
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-11-24 22:01:43 +0900

    Title: 1차원 폭발 게임, Time: 51ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241124/1차원 폭발 게임/The-1D-bomb-game.py
+++ 241124/1차원 폭발 게임/The-1D-bomb-game.py
@@ -6,24 +6,38 @@ def collapse_bombs(n, m, bombs):
         stack.append(bomb)
 
         # Check if the latest added bomb causes any explosion.
-        while len(stack) >= m:
-            top_bombs = stack[-m:]
-            if all(b == top_bombs[0] for b in top_bombs):
-                # Remove the bombs from stack
-                del stack[-m:]
-                # Check if there are more consecutive numbers to remove.
-                continue
-            else:
+        while True:
+            # Traverse the stack to identify consecutive segments of the same bomb value
+            temp_stack = []
+            i = 0
+            while i < len(stack):
+                j = i
+                while j < len(stack) and stack[j] == stack[i]:
+                    j += 1
+                # If the length of the segment is greater than or equal to m, skip adding to temp_stack (simulate explosion)
+                if j - i >= m:
+                    i = j
+                else:
+                    temp_stack.extend(stack[i:j])
+                    i = j
+            
+            # If no change, break the loop
+            if len(temp_stack) == len(stack):
                 break
+            else:
+                stack = temp_stack
 
     return stack
 
+
+# Read inputs
 n, m = map(int, input().split())
 bombs = [int(input()) for _ in range(n)]
 
 # Calculate final state of the bombs after all explosions
 result = collapse_bombs(n, m, bombs)
 
+# Output the result
 print(len(result))
 for bomb in result:
     print(bomb)
