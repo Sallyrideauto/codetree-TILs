16009c5592684a37d097188bef2be1c974026265
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-11-24 22:13:17 +0900

    Title: 알파벳과 사칙연산, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241124/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 241124/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -2,12 +2,12 @@ from itertools import product
 
 def evaluate_expression(expression, values):
     value_map = {chr(ord('a') + i): values[i] for i in range(len(values))}
-    current_value = value_map[expression[0]]
+    current_value = value_map.get(expression[0], 0)
     
     for i in range(1, len(expression), 2):
         op = expression[i]
         next_char = expression[i + 1]
-        next_value = value_map[next_char]
+        next_value = value_map.get(next_char, 0)
         if op == '+':
             current_value += next_value
         elif op == '-':
@@ -24,6 +24,7 @@ def maximize_expression(expression):
     
     # Generate all combinations of possible values for the variables
     for values in product(range(1, 5), repeat=len(unique_variables)):
+        value_map = {var: val for var, val in zip(unique_variables, values)}
         result = evaluate_expression(expression, values)
         max_result = max(max_result, result)
     
