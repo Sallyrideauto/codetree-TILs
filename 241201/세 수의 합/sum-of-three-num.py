d6bdf24e42d23dc3347b1feef27e82fab7bfb7a0
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-12-01 17:34:39 +0900

    Title: 세 수의 합, Time: 74ms, Memory: 16MB, Status: Passed - CodeTree

--- 241201/세 수의 합/sum-of-three-num.py
+++ 241201/세 수의 합/sum-of-three-num.py
@@ -1,12 +1,14 @@
-from itertools import combinations
-
 def count_combinations(nums, k):
+    n = len(nums)
     count = 0
-    # itertools의 combinations를 사용해 nums 리스트에서 3개의 원소를 고르는 모든 조합을 구합니다.
-    for comb in combinations(range(len(nums)), 3):
-        # 각 조합의 세 원소의 합이 k와 같다면 카운트를 증가시킵니다.
-        if nums[comb[0]] + nums[comb[1]] + nums[comb[2]] == k:
-            count += 1
+
+    # 세 개의 포인터를 사용하는 방식으로 문제를 해결합니다.
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):
+            for l in range(j + 1, n):
+                if nums[i] + nums[j] + nums[l] == k:
+                    count += 1
+
     return count
 
 # 입력 처리
