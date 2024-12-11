3b354f7b341e57d89bffa3f30f78c75a5b286632
Harris Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2024-12-12 05:13:04 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241212/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 241212/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -6,15 +6,15 @@ def min_operations_to_one(N):
     for i in range(2, N + 1):
         # 1을 뺄 경우
         dp[i] = dp[i - 1] + 1
-        
+
         # 2로 나누어지는 경우
         if i % 2 == 0:
             dp[i] = min(dp[i], dp[i // 2] + 1)
-        
+
         # 3으로 나누어지는 경우
         if i % 3 == 0:
             dp[i] = min(dp[i], dp[i // 3] + 1)
-    
+
     return dp[N]
 
 # 입력 받기
